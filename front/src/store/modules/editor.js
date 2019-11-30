062d5a2ee445b2984fa3ebd8b65a780bff24b17e
JoDMSoluth <JoDMSoluth@gmail.com>
2019-11-30 17:19:28 +0900

    11/30 front

--- front/src/store/modules/editor.js
+++ front/src/store/modules/editor.js
@@ -1,18 +1,22 @@
 import { createAction, handleActions } from "redux-actions";
+import * as api from "lib/api";
 
 import { Map } from "immutable";
 import { pender } from "redux-pender";
 
 const INITIALIZE = "editor/INITIALIZE";
 const CHANGE_INPUT = "editor/CHANGE_INPUT";
+const WRITE_POST = "editor/WRITE_POST";
 
 export const initialize = createAction(INITIALIZE);
 export const changeInput = createAction(CHANGE_INPUT);
+export const writePost = createAction(WRITE_POST, api.writePost);
 
 const initialState = Map({
   title: "",
   markdown: "",
-  tags: ""
+  tags: "",
+  postId: null
 });
 
 export default handleActions(
@@ -21,7 +25,14 @@ export default handleActions(
     [CHANGE_INPUT]: (state, action) => {
       const { name, value } = action.payload;
       return state.set(name, value);
-    }
+    },
+    ...pender({
+      type: WRITE_POST,
+      onSuccess: (state, action) => {
+        const { _id } = action.payload.data;
+        return state.set("postId", _id);
+      }
+    })
   },
   initialState
 );
